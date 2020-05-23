23c78aacbff7fd9886a66324f08ed6d483a08fac
Victor <vpma_amorim@hotmail.com>
2020-05-22 22:44:47 -0300

    Added responsive.css

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+let show = true;
+const menuSection = document.querySelector(".menu-section ");
+const menuToggle = menuSection.querySelector(".menu-toggle");
+
+menuToggle.addEventListener("click", () => {
+  document.body.style.overflow = show ? "hidden" : "initial";
+  menuSection.classList.toggle("collapsed", show);
+  show = !show;
+});
